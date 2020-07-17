63fc610c02e0b9e1b36b4ad6f87989e4724eb366
ganesankrishnan <ganesanrect@gmail.com>
2020-07-17 21:23:30 +0100

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  onNotify(pdt: any) {
+    window.alert('You will be notified when the product goes on sale !!! Current price is $' + pdt.price );
+  }
 }
 
 
